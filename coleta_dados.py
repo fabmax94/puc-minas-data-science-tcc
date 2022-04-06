db1197fc069696fd2271aad262af42438f517665
Fábio Alexandre <famhs@ecomp.poli.br>
2022-04-06 16:00:40 -0400

    Update coleta_dados.py

--- coleta_dados.py
+++ coleta_dados.py
@@ -24,4 +24,26 @@ def coleta_bug():
   URL = f"http://<SISTEMA_INTERNO>/issueGripd/listPage?start={DATA_INICIO}&end={DATA_FIM}"
 
   resposta = requests.get(URL)
-  resposta_parser
+  resposta_parser = BeautifulSoup(resposta.text, "html.parser")
+  tabela_parser = resposta_parser.find("table", {"id": "cphMasterMain_cphMain_lblSingleID"})
+
+  problemas = []
+  for tr in tabela_parser.find_all("tr", {"class": "spec_issue"}):
+      problemas.append({
+          "title": tr.find("span", {"class": "title_issue"}).text,
+          "device": tr.find("span", {"class": "device_issue"}).text,
+          "os_version": tr.find("span", {"class": "os_version_issue"}).text,
+          "problem": tr.find("span", {"class": "problem_description_issue"}).text,
+          "category": tr.find("span", {"class": "category_issue"}).text,
+          "register_date": tr.find("span", {"class": "register_date_issue"}).text,
+          "resolved_date": tr.find("span", {"class": "resolved_date_issue"}).text,
+          "resolved_by": tr.find("span", {"class": "resolved_by_issue"}).text,
+          "cause": tr.find("span", {"class": "cause_issue"}).text,
+          "resolution": tr.find("span", {"class": "resolution_issue"}).text,
+      })
+
+  open("/home/fabio.silva/dados/problemas.json", "w").write(json.dumps(problemas))
+  
+if __name__ == "__main__":
+  coleta_projetos()
+  coleta_bug()
